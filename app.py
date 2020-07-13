0205290c6a2668970aa2292aedc4ae81ae9c5c4f
Rasim <rasimmammadoff1@gmail.com>
2020-07-13 19:43:30 +0400

    Simple socketio chat logic

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask,render_template,request,redirect,url_for
+from flask_socketio import SocketIO,join_room
+
+app = Flask(__name__)
+socketio = SocketIO(app)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/chat')
+def chat():
+    username = request.args.get('username')
+    room = request.args.get('room')
+    if username and room:
+        return render_template('chat.html',username=username,room=room)
+    return redirect(url_for('home'))
+
+
+@socketio.on('join-room')
+def handle_join_room(data):
+    join_room(data['room'])
+    socketio.emit('join-room-info',data)
+
+
+@socketio.on('send-message')
+def handle_send_message(data):
+    socketio.emit('receive-message',data)
+
+
+
+
+if __name__ == '__main__':
+    socketio.run(app,debug=True)
