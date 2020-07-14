7e827475fa4df4b6a3f6e07586e69518cd0e5cfe
Rasim <rasimmammadoff1@gmail.com>
2020-07-14 23:53:39 +0400

    Login and signup pages were added

--- app.py
+++ app.py
@@ -1,20 +1,75 @@
-from flask import Flask,render_template,request,redirect,url_for
+from flask import Flask,render_template,request,redirect,url_for,flash
 from flask_socketio import SocketIO,join_room
+from flask_login import LoginManager, login_user,login_required,current_user,logout_user
+from db import *
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'sij%&)($*$KJLFKPOJIFH)_)!)_)@$'
+
 socketio = SocketIO(app)
+login_manager = LoginManager()
+login_manager.login_view = 'login'
+login_manager.init_app(app)
+
+
+
+@login_manager.user_loader
+def load_user(username):
+    return get_user(username)
+
 
 
 @app.route('/')
+@login_required
 def home():
     return render_template('index.html')
 
+@app.route('/login',methods=['GET','POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        user = get_user(username)
+
+        if user and user.check_password(password):
+            login_user(user)
+            flash('User loged in succesfully!')
+            return redirect(url_for('home'))
+        else:
+            flash('User cridentials are not correct!')
+    return render_template('login.html')
+
+@app.route('/signup',methods=['GET','POST'])
+def signup():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        if get_user(username):
+            flash('This username is taken')
+            return redirect(url_for('signup'))
+        else:
+            save_user(username,email,password)
+            flash('User created succesfully')
+            return redirect(url_for('login'))
+    return render_template('signup.html')
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+    
+
 @app.route('/chat')
 def chat():
-    username = request.args.get('username')
     room = request.args.get('room')
-    if username and room:
-        return render_template('chat.html',username=username,room=room)
+    if room:
+        return render_template('chat.html',room=room)
     return redirect(url_for('home'))
 
 
--- db.py
+++ db.py
@@ -1,14 +1,22 @@
 from pymongo import MongoClient
 from werkzeug.security import generate_password_hash
+from user import User
 
-
+# mongoDb connection
 client = MongoClient('mongodb+srv://helloworld:helloworld@cluster0.ebecx.mongodb.net/ChatApp?retryWrites=true&w=majority')
 
+# get database
 chat_db = client.get_database('ChatApp')
+# get table
 user_collection = chat_db.get_collection('users')
 
 def save_user(username,email,password):
     password = generate_password_hash(password)
+    # insert data
     user_collection.insert_one({'_id':username,"email":email,"password":password})
 
+def get_user(username):
+    # get data
+    user = user_collection.find_one({'_id':username})
+    return User(user['_id'],user['email'],user['password']) if user is not None else None
 
